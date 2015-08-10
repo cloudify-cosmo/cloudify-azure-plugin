bd7ec9ccd9a7e59de896b9ac3072f53ea09c911b
ruchika-satalkar <ruchika.satalkar@gmail.com>
2015-08-10 02:13:14 -0400

    Update server.py

--- azure/server.py
+++ azure/server.py
@@ -31,25 +31,25 @@ def create_resource_group(**_):
 
     resource_group_name = ctx.node.properties['resource_group_name']
     location = ctx.node.properties['location']
-    resource_group_url = constants.resource_group_url
+    resource_group_url = 'https://management.azure.com/subscriptions/'+constants.subscription_id+'/resourceGroups/'+resource_group_name+'?api-version='+constants.api_version
     ctx.logger.info("Checking availability of resource_group: " + resource_group_name)
 
-    if resource_group_name not in [resource_group_name for rg in utils.list_all_rg()]:
+    if resource_group_name not in [resource_group_name for rg in utils.list_all_resource_groups()]:
         try:
             ctx.logger.info("Creating new Resource group: " + resource_group_name)
-            rg_params=json.dumps({"name":resource_group_name,"location": location})
-            response_rg = requests.put(url=resource_group_url, data=rg_params, headers=constants.headers)
+            resource_group_params=json.dumps({"name":resource_group_name,"location": location})
+            response_rg = requests.put(url=resource_group_url, data=resource_group_params, headers=constants.headers)
             print response_rg.text
         except WindowsAzureConflictError:
-            ctx.logger.info("Resource Group " + constants.resource_group_name + " could not be created")
+            ctx.logger.info("Resource Group " + resource_group_name + " could not be created")
             sys.exit(1)
     else:
-        ctx.logger.info("Resource Group " + constants.resource_group_name + " has already been provisioned")
+        ctx.logger.info("Resource Group " + resource_group_name + " has already been provisioned")
 
 @operation
 def resource_group_creation_validation(**_):
     resource_group_name = ctx.node.properties['resource_group_name']
-    if resource_group_name in resource_group_name in [resource_group_name for rg in utils.list_all_rg()]:
+    if resource_group_name in resource_group_name in [resource_group_name for rg in utils.list_all_resource_groups()]:
         ctx.logger.info("Resource group: " + resource_group_name + " successfully created.")
     else:
         ctx.logger.info("Resource Group " + resource_group_name + " creation validation failed.")
@@ -60,15 +60,18 @@ def resource_group_creation_validation(**_):
 
 @operation
 def create_storage_account(**_):
+    location = ctx.node.properties['location']
     for property_key in constants.STORAGE_ACCOUNT_REQUIRED_PROPERTIES:
         utils.validate_node_property(property_key, ctx.node.properties)
     storage_account_name = ctx.node.properties['storage_account_name']
+    resource_group_name = ctx.node.properties['resource_group_name']
     ctx.logger.info("Checking availability of storage account: " + storage_account_name)
-    if storage_account_name not in [storage_account_name for sg in utils.list_all_sg()]:
+    if storage_account_name not in [storage_account_name for sg in utils.list_all_storage_accounts()]:
         try:
             ctx.logger.info("Creating new storage account: " + storage_account_name)
-            sg_params=json.dumps({"properties": {"accountType": constants.storage_account_type,}, "location": constants.location})
-            response_sg = requests.put(url=constants.storage_account_url, data=sg_params, headers=constants.headers)
+            storage_account_url='https://management.azure.com/subscriptions/'+constants.subscription_id+'/resourceGroups/'+resource_group_name+'/providers/Microsoft.Storage/storageAccounts/'+storage_account_name+'?api-version='+constants.api_version
+            storage_group_params=json.dumps({"properties": {"accountType": constants.storage_account_type,}, "location": location})
+            response_sg = requests.put(url=storage_account_url, data=storage_group_params, headers=constants.headers)
             print response_sg.text
         except WindowsAzureConflictError:
             ctx.logger.info("Storage Account " + storage_account_name + "could not be created.")
@@ -79,7 +82,7 @@ def create_storage_account(**_):
 @operation
 def storage_account_creation_validation(**_):
     storage_account_name = ctx.node.properties['storage_account_name']
-    if storage_account_name in [storage_account_name for sg in utils.list_all_sg()]:
+    if storage_account_name in [storage_account_name for sg in utils.list_all_storage_accounts()]:
         ctx.logger.info("Storage account: " + storage_account_name + " successfully created.")
     else:
         ctx.logger.info("Storage Account " + storage_account_name + " creation validation failed..")
@@ -93,13 +96,13 @@ def storage_account_creation_validation(**_):
 def create_vnet(**_):
     for property_key in constants.VNET_REQUIRED_PROPERTIES:
         utils.validate_node_property(property_key, ctx.node.properties)
+    resource_group_name = ctx.node.properties['resource_group_name']
+    vnet_name = ctx.node.properties['vnet_name']
+    location = ctx.node.properties['location']
+    vnet_url = 'https://management.azure.com/subscriptions/'+constants.subscription_id+'/resourceGroups/'+resource_group_name+'/providers/microsoft.network/virtualNetworks/'+vnet_name+'?api-version='+constants.api_version
+    ctx.logger.info("Checking availability of virtual network: " + vnet_name)
 
-vnet_name = ctx.node.properties['vnet_name']
-location = ctx.node.properties['location']
-vnet_url = constants.vnet_url
-ctx.logger.info("Checking availability of virtual network: " + vnet_name)
-
-if vnet_name not in [vnet_name for vnet in utils.list_all_vnet()]:
+if vnet_name not in [vnet_name for vnet in utils.list_all_vnets()]:
     try:
         ctx.logger.info("Creating new virtual network: " + vnet_name)
 
@@ -115,7 +118,7 @@ else:
 
 def vnet_creation_validation(**_):
     vnet_name = ctx.node.properties['vnet_name']
-    if vnet_name in [vnet_name for vnet in utils.list_all_vnet()]:
+    if vnet_name in [vnet_name for vnet in utils.list_all_vnets()]:
         ctx.logger.info("Virtual Network: " + vnet_name + " successfully created.")
     else:
         ctx.logger.info("Virtual Network " + vnet_name + " creation validation failed.")
@@ -125,15 +128,18 @@ def vnet_creation_validation(**_):
 #nic:
 def create_nic():
     nic_name = ctx.node.properties['nic_name']
+    resource_group_name = ctx.node.properties['resource_group_name']
+    location = ctx.node.properties['location']
+    vnet_name = ctx.node.properties['vnet_name']
     nic_params=json.dumps({
-                "location":constants.location,
+                "location":location,
                 "properties":{
                     "ipConfigurations":[
                         {
                             "name":constants.ip_config_name,
                             "properties":{
                                 "subnet":{
-                                    "id":"/subscriptions/"+constants.subscription_id+"/resourceGroups/maryland/providers/Microsoft.Network/virtualNetworks/mdvnet/subnets/Subnet-1"
+                                    "id":"/subscriptions/"+constants.subscription_id+"/resourceGroups/"+resource_group_name+"/providers/Microsoft.Network/virtualNetworks/"+vnet_name+"/subnets/Subnet-1"
                                 },
                                 "privateIPAllocationMethod":"Dynamic",
                             }
@@ -141,7 +147,9 @@ def create_nic():
                     ],
                 }
             })
-    response_nic = requests.put(url=constants.nic_url, data=nic_params, headers=constants.headers)
+    nic_url="https://management.azure.com/subscriptions/"+constants.subscription_id+"/resourceGroups/"+resource_group_name+"/providers/microsoft.network/networkInterfaces/"+nic_name+"?api-version="+constants.api_version
+
+    response_nic = requests.put(url=nic_url, data=nic_params, headers=constants.headers)
     print(response_nic.text)
 
 
@@ -149,70 +157,75 @@ def create_nic():
 
 @operation
 def create_vm(**_):
+
     for property_key in constants.VM_REQUIRED_PROPERTIES:
         utils.validate_node_property(property_key, ctx.node.properties)
-
-        vm_name = ctx.node.properties['vm_name']
-        ctx.logger.info("Checking availability of virtual network: " + vm_name)
-        if vm_name not in [vm_name for vm in utils.list_all_virtual_machines()]:
-            try:
-                ctx.logger.info("Creating new virtual machine: " + vm_name)
-
-                vm_params_1=json.dumps(
-                    {
-                        "id":"/subscriptions/"+constants.subscription_id+"/resourceGroups/maryland/providers/Microsoft.Compute/virtualMachines/new",
-                        "name":"new",
-                        "type":constants.vm_type,
-                        "location":constants.location,
-                        "properties": {
-                            "hardwareProfile": {
-                                "vmSize": constants.vm_size
-                            },
-                            "osProfile": {
-                                "computername": constants.computer_name,
-                                "adminUsername": constants.admin_username,
-                                "linuxConfiguration": {
-                                    "disablePasswordAuthentication": "true",
-                                    "ssh": {
-                                        "publicKeys": [
-                                            {
-                                                "path": constants.key_path,
-                                                "keyData": <RSA Public key here>}
-                                        ]
-                                    }
+        
+    resource_group_name = ctx.node.properties['resource_group_name']
+    storage_account_name = ctx.node.properties['storage_account_name']
+    location = ctx.node.properties['location']
+    vnet_name = ctx.node.properties['vnet_name']
+    nic_name = ctx.node.properties['nic_name']
+    vm_name = ctx.node.properties['vm_name']
+    ctx.logger.info("Checking availability of virtual network: " + vm_name)
+    if vm_name not in [vm_name for vm in utils.list_all_virtual_machines()]:
+        try:
+            ctx.logger.info("Creating new virtual machine: " + vm_name)
+                virtual_machine_params=json.dumps(
+                {
+                    "id":"/subscriptions/"+constants.subscription_id+"/resourceGroups/"+resource_group_name+"/providers/Microsoft.Compute/virtualMachines/"+vm_name
+                    "name":"new",
+                    "type":constants.vm_type,
+                    "location":location,
+                    "properties": {
+                        "hardwareProfile": {
+                            "vmSize": constants.vm_size
+                        },
+                        "osProfile": {
+                            "computername": constants.computer_name,
+                            "adminUsername": constants.admin_username,
+                            "linuxConfiguration": {
+                                "disablePasswordAuthentication": "true",
+                                "ssh": {
+                                    "publicKeys": [
+                                        {
+                                            "path": constants.key_path,
+                                            "keyData": <RSA Public key here>}
+                                    ]
                                 }
-                            },
-                            "storageProfile": {
-                                "imageReference": {
-                                    "publisher": constants.image_reference_publisher,
-                                    "offer": constants.image_reference_offer,
-                                    "sku" : constants.image_reference_sku,
-                                    "version":constants.vm_version
-                                },
-                                "osDisk" : {
-                                    "name": constants.os_disk_name,
-                                    "vhd": {
-                                        "uri": "http://ruchikastorage.blob.core.windows.net/vhds/osdisk.vhd"
-                                    },
-                                    "caching": "ReadWrite",
-                                    "createOption": "FromImage"
-                                }
-                            },
-                            "networkProfile": {
-                                "networkInterfaces": [
-                                    {
-                                        "id": "/subscriptions/"+constants.subscription_id+"/resourceGroups/maryland/providers/Microsoft.Network/networkInterfaces/nic"
-                                    }
-                                ]
                             }
+                        },
+                        "storageProfile": {
+                            "imageReference": {
+                                "publisher": constants.image_reference_publisher,
+                                "offer": constants.image_reference_offer,
+                                "sku" : constants.image_reference_sku,
+                                "version":constants.vm_version
+                            },
+                            "osDisk" : {
+                                "name": constants.os_disk_name,
+                                "vhd": {
+                                    "uri": "http://"+storage_account_name+"blob.core.windows.net/vhds/osdisk.vhd"
+                                },
+                                "caching": "ReadWrite",
+                                "createOption": "FromImage"
+                            }
+                        },
+                        "networkProfile": {
+                            "networkInterfaces": [
+                                {
+                                    "id": "/subscriptions/"+constants.subscription_id+"/resourceGroups/"+resource_group_name+"/providers/Microsoft.Network/networkInterfaces/"+nic_name
+                                }
+                            ]
                         }
-                    })
-                response_vm = requests.put(url=constants.vm_url, data=vm_params_1, headers=constants.headers)
-                print(response_vm.text)
-            except WindowsAzureConflictError:
-              ctx.logger.info("Virtual Machine " + vm_name + "could not be created.")
-              sys.exit(1)
-
+                    }
+                })
+            vm_url='https://management.azure.com/subscriptions/'+constants.subscription_id+'/resourceGroups/'+resource_group_name+'/providers/Microsoft.Compute/virtualMachines/'+vm_name+'?validating=true&api-version='+constants.api_version
+            response_vm = requests.put(url=virtual_machine_url, data=virtual_machine_params, headers=constants.headers)
+            print(response_vm.text)
+        except WindowsAzureConflictError:
+          ctx.logger.info("Virtual Machine " + vm_name + "could not be created.")
+          sys.exit(1)
     else:
      ctx.logger.info("Virtual Machine" + vm_name + "has already been provisioned by another user.")
 
