452d2e1104c45050b4f89db5035070221c252ec9
ruchika-satalkar <ruchika.satalkar@gmail.com>
2015-08-25 21:50:37 -0400

    Update utils.py

--- azure/utils.py
+++ azure/utils.py
@@ -22,6 +22,27 @@ from cloudify.exceptions import NonRecoverableError
 from cloudify.decorators import operation
 import constants
 
+@operation
+def generate_credentials():
+    client_id=ctx.node.properties('client_id')
+    tenant_id=ctx.node.properties('tenant_id')
+    username=ctx.node.properties('username')
+    password=ctx.node.properties('password')
+    url='https://login.microsoftonline.com/'+tenant_id+'/oauth2/token'
+    headers ={"Content-Type":"application/x-www-form-urlencoded"}
+    body = "grant_type=password&username="+username+"&password="+password+"&client_id="+client_id+"&resource=https://management.core.windows.net/"
+    req = Request(method="POST",url=url,data=body)
+    req_prepped = req.prepare()
+    s = Session()
+    res = Response()
+    res = s.send(req_prepped)
+    s=res.content
+    end_of_leader = s.index('access_token":"') + len('access_token":"')
+    start_of_trailer = s.index('"', end_of_leader)
+    token=s[end_of_leader:start_of_trailer]
+    print(token)
+    credentials = "Bearer " + token
+    return credentials
 
 
 @operation
