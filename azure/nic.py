98b893c3f69ec7bf3fdfab66b66e8da4af54e7d4
Vaidehi Pathak <pathak.vaidehi23@gmail.com>
2015-08-26 02:26:54 -0400

    Update nic.py

--- azure/nic.py
+++ azure/nic.py
@@ -38,7 +38,7 @@ def nic_creation_validation(**_):
 @operation
 #nic:
 def create_nic():
-    for property_key in constants.NIC_REQ_PROPERTIES:
+    for property_key in constants.NIC_REQUIRED_PROPERTIES:
        _validate_node_properties(property_key, ctx.node.properties)
     nic_name = ctx.node.properties['vm_name']+'_nic'
     resource_group_name = ctx.node.properties['vm_name']+'_resource_group'
