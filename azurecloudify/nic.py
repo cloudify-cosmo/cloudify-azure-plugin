1736aac8d3a8f1858e02cc1dc35e6cba23e8e5fa
ruchika-satalkar <ruchika.satalkar@gmail.com>
2015-08-27 01:23:06 -0400

    Update nic.py

--- azurecloudify/nic.py
+++ azurecloudify/nic.py
@@ -60,12 +60,12 @@ def create_nic(**_):
                                 ],
                             }
                         })
-           nic_url=constants.azure_url+"/subscriptions/"+subscription_id+"/resourceGroups/"+resource_group_name+"/providers/microsoft.network/networkInterfaces/"+nic_name+"?api-version="+constants.api_version
-           response_nic = requests.put(url=nic_url, data=nic_params, headers=_generate_credentials())
-           print(response_nic.text)
+          nic_url=constants.azure_url+"/subscriptions/"+subscription_id+"/resourceGroups/"+resource_group_name+"/providers/microsoft.network/networkInterfaces/"+nic_name+"?api-version="+constants.api_version
+          response_nic = requests.put(url=nic_url, data=nic_params, headers=_generate_credentials())
+          print(response_nic.text)
         except WindowsAzureConflictError:
-           ctx.logger.info("network interface card " + nic_name + "could not be created.")
-           sys.exit(1)
+          ctx.logger.info("network interface card " + nic_name + "could not be created.")
+          sys.exit(1)
     else:
      ctx.logger.info("network interface card" + nic_name + "has already been provisioned by another user.")
     
