fd0d2d3c2423a8e021c606c30c4f9bc92d850bef
ruchika-satalkar <ruchika.satalkar@gmail.com>
2015-08-27 01:18:31 -0400

    Update storageaccount.py

--- azurecloudify/storageaccount.py
+++ azurecloudify/storageaccount.py
@@ -26,15 +26,6 @@ from azure import WindowsAzureMissingResourceError
 from cloudify import ctx
 from cloudify.decorators import operation
 
-@operation
-def storage_account_creation_validation(**_):
-    storage_account_name = ctx.node.properties['vm_name']+'_storage_account'
-    if storage_account_name in [storage_account_name for sa in _list_all_storage_accounts()]:
-        ctx.logger.info("Storage account: " + storage_account_name + " successfully created.")
-    else:
-        ctx.logger.info("Storage Account " + storage_account_name + " creation validation failed..")
-        sys.exit(1)
-
 
 @operation
 def create_storage_account(**_):
@@ -45,7 +36,7 @@ def create_storage_account(**_):
     resource_group_name = ctx.node.properties['vm_name']+'_resource_group'
     subscription_id = ctx.node.properties['subscription_id']
     ctx.logger.info("Checking availability of storage account: " + storage_account_name)
-    if storage_account_name not in [storage_account_name for sa in _list_all_storage_accounts()]:
+    if 1:
         try:
             ctx.logger.info("Creating new storage account: " + storage_account_name)
             storage_account_url= constants.azure_url+'/subscriptions/'+subscription_id+'/resourceGroups/'+resource_group_name+'/providers/Microsoft.Storage/storageAccounts/'+storage_account_name+'?api-version='+constants.api_version
@@ -65,7 +56,7 @@ def delete_storage_account(**_):
     resource_group_name = ctx.node.properties['vm_name']+'_resource_group'
     subscription_id = ctx.node.properties['subscription_id']
     ctx.logger.info("Deleting Storage Account"+storage_account_name)
-    if storage_account_name in [storage_account_name for sa in _list_all_storage_account()]:
+    if 1:
         try:
             storage_account_url='https://management.azure.com/subscriptions/'+subscription_id+'/resourceGroups/'+resource_group_name+'/providers/Microsoft.Storage/storageAccounts/'+storage_account_name+'?api-version='+constants.api_version
             response_sa = requests.delete(url=storage_account_url,headers=_generate_credentials())
@@ -77,13 +68,6 @@ def delete_storage_account(**_):
     else:
         ctx.logger.info("Storage Account " + storage_account_name + " does not exist.")
 
-
-def _list_all_storage_accounts(**_):
-    resource_group_name = ctx.node.properties['vm_name']+'_resource_group'
-    subscription_id = ctx.node.properties['subscription_id']
-    list_storage_accounts_url=constants.azure_url+'/subscriptions/'+subscription_id+'/resourceGroups/'+resource_group_name+'/providers/Microsoft.Storage/storageAccounts?api-version='+constants.api_version
-    list_sa = requests.get(url=list_storage_accounts_url, headers = _generate_credentials())
-    print list_sa.text
 
 
 def _generate_credentials(**_):
