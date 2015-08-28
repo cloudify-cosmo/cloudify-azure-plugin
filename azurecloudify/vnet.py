206c513a49b67dfaf21531573626ba139be51b18
Vaidehi Pathak <pathak.vaidehi23@gmail.com>
2015-08-28 01:13:11 -0400

    Update vnet.py

--- azurecloudify/vnet.py
+++ azurecloudify/vnet.py
@@ -74,7 +74,7 @@ def delete_vnet(**_):
     else:
         ctx.logger.info("Virtual Network " + vnet_name + " does not exist.")
 
-
+"""
 def _generate_credentials(**_):
     client_id=ctx.node.properties['client_id']
     tenant_id=ctx.node.properties['tenant_id']
@@ -95,7 +95,7 @@ def _generate_credentials(**_):
     credentials = "Bearer " + token
     head = {"Content-Type": "application/json", "Authorization": credentials}
     return head
-
+"""
 
 
 def _validate_node_properties(key, ctx_node_properties):
