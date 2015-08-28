a46c18b9c57ae9740aeda23af8278accc64bc49f
Vaidehi Pathak <pathak.vaidehi23@gmail.com>
2015-08-28 01:11:56 -0400

    Update resourcegroup.py

--- azurecloudify/resourcegroup.py
+++ azurecloudify/resourcegroup.py
@@ -68,7 +68,7 @@ def delete_resource_group(**_):
     else:
         ctx.logger.info("Resource Group '%s' does not exist" + resource_group_name)
 
-
+"""
 def _generate_credentials(**_):
     client_id=ctx.node.properties['client_id']
     tenant_id=ctx.node.properties['tenant_id']
@@ -89,7 +89,7 @@ def _generate_credentials(**_):
     credentials = "Bearer " + token
     head = {"Content-Type": "application/json", "Authorization": credentials}
     return head
-
+"""
 
 
 def _validate_node_properties(key, ctx_node_properties):
