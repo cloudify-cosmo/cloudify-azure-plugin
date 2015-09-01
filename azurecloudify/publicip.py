0f9490ebec571c0857c7d8dae8b7341a222e3e19
ruchika-satalkar <ruchika.satalkar@gmail.com>
2015-09-01 00:43:33 -0400

    Update publicip.py

--- azurecloudify/publicip.py
+++ azurecloudify/publicip.py
@@ -27,7 +27,7 @@ from cloudify.decorators import operation
 @operation
 def creation_validation(**_):
     for property_key in constants.PUBLIC_IP_REQUIRED_PROPERTIES:
-        _validate_node_property(property_key, ctx.node.properties)
+        _validate_node_properties(property_key, ctx.node.properties)
 
       
 @operation
