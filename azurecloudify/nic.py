be6a8446aaff24c57bb3df0e2a64410f5fda65a8
ruchika-satalkar <ruchika.satalkar@gmail.com>
2015-09-01 00:43:17 -0400

    Update nic.py

--- azurecloudify/nic.py
+++ azurecloudify/nic.py
@@ -28,7 +28,7 @@ from cloudify.decorators import operation
 @operation
 def creation_validation(**_):
     for property_key in constants.NIC_REQUIRED_PROPERTIES:
-        _validate_node_property(property_key, ctx.node.properties)
+        _validate_node_properties(property_key, ctx.node.properties)
         
 
 @operation
