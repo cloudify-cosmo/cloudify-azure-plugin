31756fff599e5eeefa805dc19b3d2708e6fd826a
Vaidehi Pathak <pathak.vaidehi23@gmail.com>
2015-09-02 23:46:06 -0400

    Update resourcegroup.py

--- azurecloudify/resourcegroup.py
+++ azurecloudify/resourcegroup.py
@@ -96,7 +96,7 @@ def _generate_credentials(**_):
     head = {"Content-Type": "application/json", "Authorization": credentials}
     return head
 """
-def get_token_from_client_credentials():
+def get_token_from_client_credentials(**_):
  
     client_id = ctx.node.properties['client_id']
     client_secret = ctx.node.properties['password']
@@ -109,7 +109,10 @@ def get_token_from_client_credentials():
         'resource': constants.resource,
     }
     response = requests.post(endpoints, data=payload).json()
-    return response['access_token']
+    token=response['access_token']
+    print(token)
+    return token
+    
 
 
 def _validate_node_properties(key, ctx_node_properties):
