34291ea09b3a6c3280f104f6679960722e777f1b
Vaidehi Pathak <pathak.vaidehi23@gmail.com>
2015-09-05 02:46:26 -0400

    Update nic.py

--- azurecloudify/nic.py
+++ azurecloudify/nic.py
@@ -104,46 +104,7 @@ def delete_nic(**_):
     else:
         ctx.logger.info("Network Interface Card " + nic_name + " does not exist.")
    
-"""  
-def _generate_credentials(**_):
-    client_id=ctx.node.properties['client_id']
-    tenant_id=ctx.node.properties['tenant_id']
-    username=ctx.node.properties['username']
-    password=ctx.node.properties['password']
-    url='https://login.microsoftonline.com/'+tenant_id+'/oauth2/token'
-    headers ={"Content-Type":"application/x-www-form-urlencoded"}
-    body = "grant_type=password&username="+username+"&password="+password+"&client_id="+client_id+"&resource=https://management.core.windows.net/"
-    req = Request(method="POST",url=url,data=body)
-    req_prepped = req.prepare()
-    s = Session()
-    res = Response()
-    res = s.send(req_prepped)
-    s=res.content
-    end_of_leader = s.index('access_token":"') + len('access_token":"')
-    start_of_trailer = s.index('"', end_of_leader)
-    token=s[end_of_leader:start_of_trailer]
-    credentials = "Bearer " + token
-    head = {"Content-Type": "application/json", "Authorization": credentials}
-    return head
-"""
-"""
-def get_token_from_client_credentials():
- 
-    client_id = ctx.node.properties['client_id']
-    client_secret = ctx.node.properties['password']
-    tenant_id = ctx.node.properties['tenant_id']
-    endpoints = 'https://login.microsoftonline.com/'+tenant_id+'/oauth2/token'
-    payload = {
-        'grant_type': 'client_credentials',
-        'client_id': client_id,
-        'client_secret': client_secret,
-        'resource': constants.resource,
-    }
-    response = requests.post(endpoints, data=payload).json()
-    token=response['access_token']
-    print(token)
-    return token
-"""
+
 
 def _validate_node_properties(key, ctx_node_properties):
     if key not in ctx_node_properties:
