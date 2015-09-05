6739b02b7988e2ba256a616763bc202bce1c3a8a
Vaidehi Pathak <pathak.vaidehi23@gmail.com>
2015-09-05 02:50:37 -0400

    Update resourcegroup.py

--- azurecloudify/resourcegroup.py
+++ azurecloudify/resourcegroup.py
@@ -23,6 +23,7 @@ import os
 from cloudify.exceptions import NonRecoverableError
 from cloudify import ctx
 from cloudify.decorators import operation
+import auth
 
 @operation
 def creation_validation(**_):
@@ -37,10 +38,10 @@ def create_resource_group(**_):
     resource_group_name = vm_name+'_resource_group'
     location = ctx.node.properties['location']
     subscription_id = ctx.node.properties['subscription_id']
-    """
-    credentials='Bearer '+ get_token_from_client_credentials()
+    
+    credentials='Bearer '+ auth.get_token_from_client_credentials()
     headers = {"Content-Type": "application/json", "Authorization": credentials}
-    """
+   
     resource_group_url = constants.azure_url+'/subscriptions/'+subscription_id+'/resourceGroups/'+resource_group_name+'?api-version='+constants.api_version_resource_group
     ctx.logger.info("Checking availability of resource_group: " + resource_group_name)
 
@@ -48,7 +49,7 @@ def create_resource_group(**_):
         try:
             ctx.logger.info("Creating new Resource group: " + resource_group_name)
             resource_group_params=json.dumps({"name":resource_group_name,"location": location})
-            response_rg = requests.put(url=resource_group_url, data=resource_group_params, headers=constants.headers)
+            response_rg = requests.put(url=resource_group_url, data=resource_group_params, headers=headers)
             print response_rg.text
         except:
             ctx.logger.info("Resource Group " + resource_group_name + " could not be created")
@@ -62,15 +63,15 @@ def delete_resource_group(**_):
     vm_name=ctx.node.properties['vm_name']
     resource_group_name = vm_name+'_resource_group'
     subscription_id = ctx.node.properties['subscription_id']
-    """
+    
     credentials='Bearer '+get_token_from_client_credentials()
     headers = {"Content-Type": "application/json", "Authorization": credentials}
-    """
+    
     if 1:
         try:
             ctx.logger.info("Deleting Resource Group: " + resource_group_name)
             resource_group_url = 'https://management.azure.com/subscriptions/'+subscription_id+'/resourceGroups/'+resource_group_name+'?api-version='+constants.api_version_resource_group
-            response_rg = requests.delete(url=resource_group_url, headers=constants.headers)
+            response_rg = requests.delete(url=resource_group_url, headers=headers)
             print(response_rg.text)
         except:
             ctx.logger.info("Resource Group" +  resource_group_name + "could not be deleted." )
@@ -78,46 +79,6 @@ def delete_resource_group(**_):
     else:
         ctx.logger.info("Resource Group '%s' does not exist" + resource_group_name)
 
-"""
-def _generate_credentials(**_):
-    client_id=ctx.node.properties['client_id']
-    tenant_id=ctx.node.properties['tenant_id']
-    username=ctx.node.properties['username']
-    password=ctx.node.properties['password']
-    url='https://login.microsoftonline.com/'+tenant_id+'/oauth2/token'
-    headers ={"Content-Type":"application/x-www-form-urlencoded"}
-    body = "grant_type=password&username="+username+"&password="+password+"&client_id="+client_id+"&resource=https://management.core.windows.net/"
-    req = Request(method="POST",url=url,data=body)
-    req_prepped = req.prepare()
-    s = Session()
-    res = Response()
-    res = s.send(req_prepped)
-    s=res.content
-    end_of_leader = s.index('access_token":"') + len('access_token":"')
-    start_of_trailer = s.index('"', end_of_leader)
-    token=s[end_of_leader:start_of_trailer]
-    credentials = "Bearer " + token
-    head = {"Content-Type": "application/json", "Authorization": credentials}
-    return head
-"""
-
-"""
-def get_token_from_client_credentials(**_):
- 
-    client_id = ctx.node.properties['client_id']
-    client_secret = ctx.node.properties['password']
-    tenant_id = ctx.node.properties['tenant_id']
-    endpoints = 'https://login.microsoftonline.com/'+tenant_id+'/oauth2/token'
-    payload = {
-        'grant_type': 'client_credentials',
-        'client_id': client_id,
-        'client_secret': client_secret,
-        'resource': constants.resource,
-    }
-    response = requests.post(endpoints, data=payload).json()
-    token=response['access_token']
-    return token
-"""
 def _validate_node_properties(key, ctx_node_properties):
     if key not in ctx_node_properties:
         raise NonRecoverableError('{0} is a required input. Unable to create.'.format(key))
