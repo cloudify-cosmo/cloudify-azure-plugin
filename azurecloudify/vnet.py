981bb6ecc4e0cd90e9fe45dae19712c186b2ced6
Vaidehi Pathak <pathak.vaidehi23@gmail.com>
2015-09-05 00:38:11 -0400

    Update vnet.py

--- azurecloudify/vnet.py
+++ azurecloudify/vnet.py
@@ -121,7 +121,7 @@ def get_token_from_client_credentials(**_):
         'resource': constants.resource,
     }
     response = requests.post(endpoints, data=payload).json()
-    token=response.get('access_token')
+    token=response['access_token']
     print(token)
     return token
    
