819161a69e53ce2ffc9a15ec69cb04ce88e914a7
Vaidehi Pathak <pathak.vaidehi23@gmail.com>
2015-09-05 02:48:35 -0400

    Update publicip.py

--- azurecloudify/publicip.py
+++ azurecloudify/publicip.py
@@ -23,6 +23,7 @@ import os
 from cloudify.exceptions import NonRecoverableError
 from cloudify import ctx
 from cloudify.decorators import operation 
+import auth
 
 @operation
 def creation_validation(**_):
@@ -40,10 +41,10 @@ def create_public_ip(**_):
     location = ctx.node.properties['location']
     resource_group_name = ctx.node.properties['vm_name']+'_resource_group'
     public_ip_url=constants.azure_url+'/subscriptions/'+subscription_id+'/resourceGroups/'+resource_group_name+'/providers/microsoft.network/publicIPAddresses/'+public_ip_name+'?api-version='+constants.api_version
-    """
-    credentials='Bearer '+get_token_from_client_credentials()
+    
+    credentials='Bearer '+auth.get_token_from_client_credentials()
     headers = {"Content-Type": "application/json", "Authorization": credentials}
-    """
+   
     if 1:
         try:
             ctx.logger.info("Creating new public ip : " + public_ip_name)
@@ -71,7 +72,7 @@ def delete_public_ip(**_):
     public_ip_name = vm_name+'_pip'
     subscription_id = ctx.node.properties['subscription_id']
     resource_group_name = vm_name+'_resource_group'
-    credentials='Bearer '+get_token_from_client_credentials()
+    credentials='Bearer '+ auth.get_token_from_client_credentials()
     headers = {"Content-Type": "application/json", "Authorization": credentials}
     if 1:
         try:
@@ -85,48 +86,6 @@ def delete_public_ip(**_):
         
     else:
         ctx.logger.info("Public IP " + public_ip_name + " does not exist.")
-    
-"""
-def _generate_credentials(**_):
-    client_id=ctx.node.properties['client_id']
-    tenant_id=ctx.node.properties['tenant_id']
-    username=ctx.node.properties['username']
-    password=ctx.node.properties['password']
-    url='https://login.microsoftonline.com/'+tenant_id+'/oauth2/token'
-    headers ={"Content-Type":"application/x-www-form-urlencoded"}
-    body = "grant_type=password&username="+username+"&password="+password+"&client_id="+client_id+"&resource=https://management.core.windows.net/"
-    req = Request(method="POST",url=url,data=body)
-    req_prepped = req.prepare()
-    s = Session()
-    res = Response()
-    res = s.send(req_prepped)
-    s=res.content
-    end_of_leader = s.index('access_token":"') + len('access_token":"')
-    start_of_trailer = s.index('"', end_of_leader)
-    token=s[end_of_leader:start_of_trailer]
-    credentials = "Bearer " + token
-    head = {"Content-Type": "application/json", "Authorization": credentials}
-    return head
-"""
-
-"""
-def get_token_from_client_credentials(**_):
- 
-    client_id = ctx.node.properties['client_id']
-    client_secret = ctx.node.properties['password']
-    tenant_id = ctx.node.properties['tenant_id']
-    endpoints = 'https://login.microsoftonline.com/'+tenant_id+'/oauth2/token'
-    payload = {
-        'grant_type': 'client_credentials',
-        'client_id': client_id,
-        'client_secret': client_secret,
-        'resource': constants.resource,
-    }
-    response = requests.post(endpoints, data=payload).json()
-    token=response['access_token']
-    print(token)
-    return token
- """  
 
 
 def _validate_node_properties(key, ctx_node_properties):
