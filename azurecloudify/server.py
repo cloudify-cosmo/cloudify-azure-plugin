a72be9397caa6b001acaf699863cf7d5631e33e6
Vaidehi Pathak <pathak.vaidehi23@gmail.com>
2015-09-05 15:15:08 -0400

    Update server.py

--- azurecloudify/server.py
+++ azurecloudify/server.py
@@ -45,9 +45,10 @@ def create_vm(**_):
     location = ctx.node.properties['location']
     vnet_name = vm_name+'_vnet'
     nic_name = vm_name+'_nic'
-    
+    """
     credentials='Bearer '+get_token_from_client_credentials()
-    headers = {"Content-Type": "application/json", "Authorization": credentials}
+    """
+    headers = {"Content-Type": "application/json", "Authorization": constants.credentials}
     
     
     subscription_id = ctx.node.properties['subscription_id']
@@ -133,9 +134,10 @@ def start_vm(**_):
 def stop_vm(**_):
     subscription_id = ctx.node.properties['subscription_id']
     vm_name = ctx.node.properties['vm_name']
-    
+    """
     credentials='Bearer '+auth.get_token_from_client_credentials()
-    headers = {"Content-Type": "application/json", "Authorization": credentials}
+    """
+    headers = {"Content-Type": "application/json", "Authorization": constants.credentials}
     
     resource_group_name = vm_name+'_resource_group'
     stop_vm_url=constants.azure_url+'/subscriptions/'+subscription_id+'/resourceGroups/'+resource_group_name+'/providers/Microsoft.Compute/virtualMachines/'+vm_name+'/start?api-version='+constants.api_version
