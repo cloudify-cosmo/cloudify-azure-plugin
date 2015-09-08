9c090f6912c13a76cc591a63fcd4d81978ba4be7
Vaidehi Pathak <pathak.vaidehi23@gmail.com>
2015-09-08 13:08:01 -0400

    Update resourcegroup.py

--- azurecloudify/resourcegroup.py
+++ azurecloudify/resourcegroup.py
@@ -38,10 +38,10 @@ def create_resource_group(**_):
     resource_group_name = vm_name+'_resource_group'
     location = ctx.node.properties['location']
     subscription_id = ctx.node.properties['subscription_id']
-    """
+    
     credentials='Bearer '+ auth.get_token_from_client_credentials()
-    """
-    headers = {"Content-Type": "application/json", "Authorization": constants.credentials}
+    
+    headers = {"Content-Type": "application/json", "Authorization": credentials}
    
     resource_group_url = constants.azure_url+'/subscriptions/'+subscription_id+'/resourceGroups/'+resource_group_name+'?api-version='+constants.api_version_resource_group
     ctx.logger.info("Checking availability of resource_group: " + resource_group_name)
@@ -64,10 +64,10 @@ def delete_resource_group(**_):
     vm_name=ctx.node.properties['vm_name']
     resource_group_name = vm_name+'_resource_group'
     subscription_id = ctx.node.properties['subscription_id']
-    """
+    
     credentials='Bearer '+auth.get_token_from_client_credentials()
-    """
-    headers = {"Content-Type": "application/json", "Authorization": constants.credentials}
+    
+    headers = {"Content-Type": "application/json", "Authorization": credentials}
     
     if 1:
         try:
