9e1779573659bc12bc34cab923021b6da169482d
tamirko <tamir@gigaspaces.com>
2015-09-10 20:23:09 +0300

    replaced a redundant ctx access

--- azurecloudify/publicip.py
+++ azurecloudify/publicip.py
@@ -40,7 +40,7 @@ def create_public_ip(**_):
     public_ip_name=vm_name+'_pip'
     subscription_id = ctx.node.properties['subscription_id']
     location = ctx.node.properties['location']
-    resource_group_name = ctx.node.properties['vm_name']+'_resource_group'
+    resource_group_name = vm_name+'_resource_group'
     public_ip_url=constants.azure_url+'/subscriptions/'+subscription_id+'/resourceGroups/'+resource_group_name+'/providers/microsoft.network/publicIPAddresses/'+public_ip_name+'?api-version='+constants.api_version
     
     credentials='Bearer '+ auth.get_token_from_client_credentials()
