78ddca69ee2cc0acf20b4fbeb2d7aa401c6440ba
Vaidehi Pathak <pathak.vaidehi23@gmail.com>
2015-09-12 16:06:51 -0400

    Update auth.py

--- azurecloudify/auth.py
+++ azurecloudify/auth.py
@@ -8,13 +8,13 @@ import constants
 def get_token_from_client_credentials():
  
     client_id = ctx.node.properties['client_id']
-    password = ctx.node.properties['aad_password']
+    aad_password = ctx.node.properties['aad_password']
     tenant_id = ctx.node.properties['tenant_id']
     endpoints = 'https://login.microsoftonline.com/'+tenant_id+'/oauth2/token'
     payload = {
         'grant_type': 'client_credentials',
         'client_id': client_id,
-        'client_secret': password,
+        'client_secret': aad_password,
         'resource': constants.resource,
     }
     response = requests.post(endpoints, data=payload).json()
