0df477030e02205778325e2c27a83c8334ab0d74
Vaidehi Pathak <pathak.vaidehi23@gmail.com>
2015-09-13 18:03:24 -0400

    Update server.py

--- azurecloudify/server.py
+++ azurecloudify/server.py
@@ -53,8 +53,8 @@ def create_vm(**_):
         
     get_pip_info_url= 'https://management.azure.com/subscriptions/'+subscription_id+'/resourceGroups/'+resource_group_name+'/providers/microsoft.network/publicIPAddresses/'+public_ip_name+'?api-version='+constants.api_version
     response_get_info= requests.get(url=get_pip_info_url, headers=headers).json()
-    #print (response_get_info)
-    print(response_get_info['properties']['ipAddress'])
+    print (response_get_info)
+    print response_get_info['properties']['ipAddress']
     #ctx.instance.runtime_properties['vm_public_ip']= response_get_info['properties']['ipAddress']
     #ctx.source.instance.runtime_properties['vm_public_ip']=response_get_info['properties']['ipAddress']
     
