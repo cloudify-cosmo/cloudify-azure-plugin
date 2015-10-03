25704805c24bde3f490c9e29abddf80d6b6a73f2
Pranjali <pranjalib@users.noreply.github.com>
2015-10-03 04:08:53 -0400

    Update resourcegroup.py

--- azurecloudify/resourcegroup.py
+++ azurecloudify/resourcegroup.py
@@ -76,6 +76,7 @@ def create_resource_group(**_):
 	            resource_group_params=json.dumps({"name":resource_group_name,"location": location})
 	            response_rg = requests.put(url=resource_group_url, data=resource_group_params, headers=headers)
 	            print response_rg.text
+	            ctx.instance.runtime_properties['resource_group']=resource_group_name
 	        except:
 	            ctx.logger.info("Resource Group " + resource_group_name + " could not be created")
 	            sys.exit(1)
