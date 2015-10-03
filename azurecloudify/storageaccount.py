864c6ed2f4133173141f6352d1eee11bb0e4255e
Pranjali <pranjalib@users.noreply.github.com>
2015-10-03 02:39:53 -0400

    Update storageaccount.py

--- azurecloudify/storageaccount.py
+++ azurecloudify/storageaccount.py
@@ -21,42 +21,51 @@ import constants
 import sys
 import os
 import auth
+import utils
 from cloudify.exceptions import NonRecoverableError
 from cloudify import ctx
 from cloudify.decorators import operation
 
 @operation
 def creation_validation(**_):
-    for property_key in constants.STORAGE_ACCOUNT_REQUIRED_PROPERTIES:
-        _validate_node_properties(property_key, ctx.node.properties)
+    
 
 @operation
 def create_storage_account(**_):
-    location = ctx.node.properties['location']
-    for property_key in constants.STORAGE_ACCOUNT_REQUIRED_PROPERTIES:
-       _validate_node_properties(property_key, ctx.node.properties)
-    vm_name=ctx.node.properties['vm_name']
-    storage_account_name = vm_name+'storageaccount'
-    resource_group_name = vm_name+'_resource_group'
-    subscription_id = ctx.node.properties['subscription_id']
-    
-    credentials='Bearer '+auth.get_token_from_client_credentials()
-    
-    headers = {"Content-Type": "application/json", "Authorization": credentials}
-    
-    ctx.logger.info("Checking availability of storage account: " + storage_account_name)
-    if 1:
-        try:
-            ctx.logger.info("Creating new storage account: " + storage_account_name)
-            storage_account_url= constants.azure_url+'/subscriptions/'+subscription_id+'/resourceGroups/'+resource_group_name+'/providers/Microsoft.Storage/storageAccounts/'+storage_account_name+'?api-version='+constants.api_version
-            storage_account_params=json.dumps({"properties": {"accountType": constants.storage_account_type,}, "location": location})
-            response_sa = requests.put(url=storage_account_url, data=storage_account_params, headers=headers)
-            print response_sa.text
-        except:
-            ctx.logger.info("Storage Account " + storage_account_name + "could not be created.")
+    if ctx.node.properties['use_external_resource']
+        if not resource_group:
+            raise NonRecoverableError(
+            'External resource, but the supplied '
+            'resource group does not exist in the account.')
             sys.exit(1)
-    else:
-        ctx.logger.info("Storage Account " + storage_account_name + "has already been provisioned by another user.")
+        else
+            ctx.instance.runtime_properties['existing_resource_group_name']
+    else
+            for property_key in constants.STORAGE_ACCOUNT_REQUIRED_PROPERTIES:
+                _validate_node_properties(property_key, ctx.node.properties)
+            location = ctx.node.properties['location']
+            vm_name=ctx.node.properties['vm_name']
+            RANDOM_SUFFIX_VALUE = utils.random_suffix_generator()
+            storage_account_name = vm_name+'storageaccount'+RANDOM_SUFFIX_VALUE
+            subscription_id = ctx.node.properties['subscription_id']
+            
+            credentials='Bearer '+auth.get_token_from_client_credentials()
+            
+            headers = {"Content-Type": "application/json", "Authorization": credentials}
+            
+            ctx.logger.info("Checking availability of storage account: " + storage_account_name)
+            if 1:
+                try:
+                    ctx.logger.info("Creating new storage account: " + storage_account_name)
+                    storage_account_url= constants.azure_url+'/subscriptions/'+subscription_id+'/resourceGroups/'+resource_group_name+'/providers/Microsoft.Storage/storageAccounts/'+storage_account_name+'?api-version='+constants.api_version
+                    storage_account_params=json.dumps({"properties": {"accountType": constants.storage_account_type,}, "location": location})
+                    response_sa = requests.put(url=storage_account_url, data=storage_account_params, headers=headers)
+                    print response_sa.text
+                except:
+                    ctx.logger.info("Storage Account " + storage_account_name + "could not be created.")
+                    sys.exit(1)
+            else:
+                ctx.logger.info("Storage Account " + storage_account_name + "has already been provisioned by another user.")
 
 
 @operation
