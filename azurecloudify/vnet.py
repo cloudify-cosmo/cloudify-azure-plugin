48dcc4aeb208b0c4b32f5e0a367be1761ab473d8
Pranjali <pranjalib@users.noreply.github.com>
2015-10-05 02:26:39 -0400

    Update vnet.py

--- azurecloudify/vnet.py
+++ azurecloudify/vnet.py
@@ -35,7 +35,7 @@ def creation_validation(**_):
     for property_key in constants.VNET_REQUIRED_PROPERTIES:
         _validate_node_properties(property_key, ctx.node.properties)
     
-    vnet_name = _get_resource_group_name(utils.get_vnet_name())
+    vnet_name = _get_resource_group_name()
     if ctx.node.properties['use_external_resource'] and not vnet_name:
     raise NonRecoverableError(
     'External resource, but the supplied '
