cb13980844c7c304338f0ecd59616ae550227083
Ruchika Satalkar <ruchika.satalkar@gmail.com>
2015-10-06 03:16:34 -0400

    Update auth.py

--- azurecloudify/auth.py
+++ azurecloudify/auth.py
@@ -17,7 +17,23 @@ def get_token_from_client_credentials():
         'client_secret': aad_password,
         'resource': constants.resource,
     }
-    response = requests.post(endpoints, data=payload).json()
-    return response['access_token']
+    path = '/root/token.txt'
+    try:
+        with open(path, 'r') as f:
+            e = f.readline()
+            token = f.readline()
+    except:
+        print 'no token file'
+        e = 0
+        token = None
+    #open file and check, extract both
+    timestamp = int(time.time())
+    if(e-timestamp <= 600 or e == 0 or token == None):
+        response = requests.post(endpoints, data=payload).json()
+        token = response['access_token']
+        e = response['expires_on']
+        with open(path, 'w+') as f:
+            f.writelines([e, '\n', token])
+    return token
 
 
