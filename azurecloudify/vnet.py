726c68b432d0004e06f399c296b2a7c1382c3b68
Pranjali <pranjalib@users.noreply.github.com>
2015-10-05 23:37:42 -0400

    Update vnet.py

--- azurecloudify/vnet.py
+++ azurecloudify/vnet.py
@@ -117,6 +117,7 @@ def _validate_node_properties(key, ctx_node_properties):
 def _get_vnet_name():
     vnet_name= ctx.node.properties['existing_vnet_name']
     resource_group_name= resourcegroup.resource_group_name
+    credentials=auth.get_token_from_client_credentials()
     subscription_id=ctx.node.properties['subscription_id']
     url = constants.azure_url+'/subscriptions/'+subscription_id+'/resourceGroups/'+resource_group_name+'/providers/microsoft.network/virtualnetworks?api-version='+constants.api_version
     headers = {"Content-Type": "application/json", "Authorization": credentials}
