d9e6e4b7dfc7ae61bd8036e4689ce62cb2c2a27a
Pranjali <pranjalib@users.noreply.github.com>
2015-10-08 01:58:46 -0400

    Update resourcegroup.py

--- azurecloudify/resourcegroup.py
+++ azurecloudify/resourcegroup.py
@@ -34,13 +34,13 @@ def creation_validation(**_):
     for property_key in constants.RESOURCE_GROUP_REQUIRED_PROPERTIES:
         _validate_node_properties(property_key, ctx.node.properties)
     
-    resource_group =  _get_resource_group_name()
+    resource_group_exists =  _get_resource_group_name()
 
-    if ctx.node.properties['use_external_resource'] and not resource_group:
+    if ctx.node.properties['use_external_resource'] and not resource_group_exists:
 	raise NonRecoverableError(
 	'External resource, but the supplied '
 	'resource group does not exist in the account.')
-    if not ctx.node.properties['use_external_resource'] and resource_group:
+    if not ctx.node.properties['use_external_resource'] and resource_group_exists:
 	raise NonRecoverableError(
 	'Not external resource, but the supplied '
 	'resource group exists in the account.')
