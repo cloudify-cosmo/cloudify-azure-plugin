bc643b0e664eeaeb618d76867dc8a54e59f6a3b9
Vaidehi Pathak <pathak.vaidehi23@gmail.com>
2015-10-09 01:13:35 -0400

    Update storageaccount.py

--- azurecloudify/storageaccount.py
+++ azurecloudify/storageaccount.py
@@ -80,7 +80,7 @@ def create_storage_account(**_):
 
 @operation
 def delete_storage_account(**_):
-    resource_group_name = resourcegroup.resource_group_name
+    resource_group_name = ctx.instance.runtime_properties['resource_group']
     subscription_id = ctx.node.properties['subscription_id']
     credentials='Bearer '+auth.get_token_from_client_credentials()
     headers = {"Content-Type": "application/json", "Authorization": credentials}
@@ -110,7 +110,7 @@ def _validate_node_properties(key, ctx_node_properties):
         
 def _get_storage_account_name():
     storage_account_name= ctx.node.properties['existing_storage_account_name']
-    resource_group_name= resourcegroup.resource_group_name
+    resource_group_name= ctx.instance.runtime_properties['resource_group']
     subscription_id=ctx.node.properties['subscription_id']
     credentials=auth.get_token_from_client_credentials()
     url = constants.azure_url+'/subscriptions/'+subscription_id+'/resourceGroups/'+resource_group_name+'/providers/Microsoft.Storage/storageAccounts?api-version='+constants.api_version
