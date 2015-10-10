02facd95bac1d22107083a81f4f4393ef515a5fb
Pranjali <pranjalib@users.noreply.github.com>
2015-10-10 02:18:58 -0400

    Update publicip.py

--- azurecloudify/publicip.py
+++ azurecloudify/publicip.py
@@ -105,7 +105,7 @@ def delete_public_ip(**_):
         
 @operation
 def set_dependent_resources_names(azure_config,**kwargs):
-   ctx.source.instance.runtime_properties['RESOURCE_GROUP_KEY'] = ctx.target.instance.runtime_properties['RESOURCE_GROUP_KEY']
+   ctx.source.instance.runtime_properties[constants.RESOURCE_GROUP_KEY] = ctx.target.instance.runtime_properties[constants.RESOURCE_GROUP_KEY]
 
 
 def _validate_node_properties(key, ctx_node_properties):
