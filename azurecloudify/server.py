c792f3b808f097ba9ef55b072fc6d82864992d2a
tamirko <tamir@gigaspaces.com>
2015-10-20 14:20:14 +0300

    Added _set_public_ip

--- azurecloudify/server.py
+++ azurecloudify/server.py
@@ -121,7 +121,7 @@ def start_vm(**_):
     resource_group_name = ctx.instance.runtime_properties[constants.RESOURCE_GROUP_KEY]
 
     if constants.PUBLIC_IP_KEY in ctx.instance.runtime_properties:
-        _set_private_ip(subscription_id, resource_group_name, headers)
+        _set_public_ip(subscription_id, resource_group_name, headers)
 
     start_vm_url = constants.azure_url+'/subscriptions/'+subscription_id+'/resourceGroups/'+resource_group_name+'/providers/Microsoft.Compute/virtualMachines/'+vm_name+'/start?api-version='+constants.api_version
     response_start_vm = requests.post(start_vm_url, headers=headers)
@@ -183,7 +183,7 @@ def _validate_node_properties(key, ctx_node_properties):
         raise NonRecoverableError('{0} is a required input. Unable to create.'.format(key))
 
 
-def _set_private_ip(subscription_id,resource_group_name,headers):
+def _set_public_ip(subscription_id,resource_group_name,headers):
     public_ip_name = ctx.instance.runtime_properties[constants.PUBLIC_IP_KEY]
     get_pip_info_url = constants.azure_url+'/subscriptions/'+subscription_id+'/resourceGroups/'+resource_group_name+'/providers/microsoft.network/publicIPAddresses/'+public_ip_name+'?api-version='+constants.api_version
     raw_response = requests.get(url=get_pip_info_url, headers=headers)
