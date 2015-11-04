9c79d6ca31562b62adea1b545732f379ff1a789e
Vaidehi Pathak <pathak.vaidehi23@gmail.com>
2015-11-04 18:19:35 -0500

    Update availabilityset.py

--- azurecloudify/availabilityset.py
+++ azurecloudify/availabilityset.py
@@ -1,20 +1,54 @@
-import json
+########
+# Copyright (c) 2015 GigaSpaces Technologies Ltd. All rights reserved
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+#    * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    * See the License for the specific language governing permissions and
+#    * limitations under the License.
+
+# Built-in Imports
 import requests
+from requests import Request,Session,Response
+import json
+import constants
+import sys
+import os
+from resourcegroup import *
+from cloudify.exceptions import NonRecoverableError,RecoverableError
+from cloudify import ctx
+from cloudify.decorators import operation
+
+@operation
+def creation_validation(**_):
+    for property_key in constants.STORAGE_ACCOUNT_REQUIRED_PROPERTIES:
+        _validate_node_properties(property_key, ctx.node.properties)
 
 
+@operation
+def create_storage_account(**_):
+    location = ctx.node.properties['location']
+    subscription_id = ctx.node.properties['subscription_id']
+    resource_group_name = ctx.instance.runtime_properties[constants.RESOURCE_GROUP_KEY]
+    availability_set_name= ''
+    credentials = 'Bearer '+auth.get_auth_token()
 
-location = ''
-subscription_id = ''
-resource_group_name = ''
-availability_set_name= ''
-credentials = 'Bearer ' + auth.get_auth_token()
-headers = {"Content-Type": "application/json", "Authorization": credentials}
-availability_set_url = 'https://management.azure.com/subscriptions/'+subscription_id+'/resourceGroups/'+resource_group_name+'/providers/Microsoft.Compute/availabilitySets/'+availability_set_name+'?api-version='
-availability_set_params = json.dumps({ 
-   "name": availability_set_name, 
-   "type": "Microsoft.Compute/availabilitySets", 
-   "location": location
-}
-)
-response_as = requests.put(url=availability_set_url, data=availability_set_params, headers=headers) 
-print response_as.text 
+    headers = {"Content-Type": "application/json", "Authorization": credentials}
+
+    credentials = 'Bearer ' + auth.get_auth_token()
+    headers = {"Content-Type": "application/json", "Authorization": credentials}
+    availability_set_url = 'https://management.azure.com/subscriptions/'+subscription_id+'/resourceGroups/'+resource_group_name+'/providers/Microsoft.Compute/availabilitySets/'+availability_set_name+'?api-version='
+    availability_set_params = json.dumps({ 
+       "name": availability_set_name, 
+       "type": "Microsoft.Compute/availabilitySets", 
+       "location": location
+    }
+    )
+    response_as = requests.put(url=availability_set_url, data=availability_set_params, headers=headers) 
+    print response_as.text 
