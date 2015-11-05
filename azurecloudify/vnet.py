594df789799be3d9078ba4e974e3268b634e0751
tamirko <tamir@gigaspaces.com>
2015-11-05 11:33:57 +0200

    fixed the vnet relationship

--- azurecloudify/vnet.py
+++ azurecloudify/vnet.py
@@ -96,8 +96,11 @@ def delete_current_vnet(**_):
 
 @operation
 def set_dependent_resources_names(azure_config, **kwargs):
-    ctx.source.instance.runtime_properties[constants.RESOURCE_GROUP_KEY] = ctx.target.instance.runtime_properties[constants.RESOURCE_GROUP_KEY]
-    ctx.source.instance.runtime_properties[constants.SECURITY_GROUP_KEY] = ctx.target.instance.runtime_properties[constants.SECURITY_GROUP_KEY]
+    if constants.RESOURCE_GROUP_KEY not in ctx.source.instance.runtime_properties:
+        ctx.source.instance.runtime_properties[constants.RESOURCE_GROUP_KEY] = ctx.target.instance.runtime_properties[constants.RESOURCE_GROUP_KEY]
+
+    if constants.SECURITY_GROUP_KEY in ctx.target.instance.runtime_properties:
+        ctx.source.instance.runtime_properties[constants.SECURITY_GROUP_KEY] = ctx.target.instance.runtime_properties[constants.SECURITY_GROUP_KEY]
 
 def _validate_node_properties(key, ctx_node_properties):
     if key not in ctx_node_properties:
