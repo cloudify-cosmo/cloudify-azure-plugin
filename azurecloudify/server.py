134afb12063d2288db3e3c6ad458e3d0400db397
Vaidehi Pathak <pathak.vaidehi23@gmail.com>
2015-11-07 23:59:44 -0500

    Update server.py

--- azurecloudify/server.py
+++ azurecloudify/server.py
@@ -219,7 +219,7 @@ def _start_vm_call(headers, vm_name, subscription_id, resource_group_name):
 def get_virtual_machine_params(location, random_suffix_value, resource_group_name, storage_account_name,
                                subscription_id, vm_name):
     vm_json = _get_vm_base_json(location, random_suffix_value, resource_group_name, storage_account_name,
-                                subscription_id, vm_name, as_name)
+                                subscription_id, vm_name)
 
     vm_properties = vm_json['properties']
     network_profile = vm_properties['networkProfile']
@@ -249,10 +249,10 @@ def _get_vm_base_json(location, random_suffix_value, resource_group_name, storag
         "name": vm_name,
         "type": "Microsoft.Compute/virtualMachines",
         "location": location,
-        "properties": {
+        "properties": """{
             "availabilitySet": { "id": "/subscriptions/{0}/resourceGroups/{1}/providers/Microsoft.Compute/availabilitySets/{2}".format(
             subscription_id, resource_group_name, as_name)
-            },
+            }"""
             "hardwareProfile": {
                 "vmSize": ctx.node.properties['vm_size']
             },
