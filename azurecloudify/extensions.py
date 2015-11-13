1a4dc05882a9a562dbf2b436006fd07cbe0f26a9
Ruchika Satalkar <ruchika.satalkar@gmail.com>
2015-11-12 16:14:14 -0800

    deleted get_info and delete methods and updated create method

--- azurecloudify/extensions.py
+++ azurecloudify/extensions.py
@@ -36,13 +36,13 @@ def create_custom_script(**_):
     custom_script_name = ''
     headers, location, subscription_id = auth.get_credentials()
     resource_group_name = ctx.instance.runtime_properties[constants.RESOURCE_GROUP_KEY]
-    command = ''
-    file_uri = ''
-    vm_name = ''
+    command = ctx.node.properties['custom_script_command']
+    file_uri = ctx.node.properties['custom_script_path']
+    vm_name = ctx.instance.runtime_properties[constants.VM_KEY]
 
-    custom_script_url = constants.azure_url+'/subscriptions/'+subscription_id+'/resourceGroups/'+resource_group_name+'/providersMicrosoft.Compute/virtualMachines/'+vm_name+'/extensions/'+custom_script_name+'?api-version=2015-05-01-preview'
+    custom_script_url = constants.azure_url+'/subscriptions/'+subscription_id+'/resourceGroups/'+resource_group_name+'/providers/Microsoft.Compute/virtualMachines/'+vm_name+'/extensions/'+custom_script_name+'?api-version='+constants.api_version
     custom_script_params = json.dumps({ 
-       "id":"/subscriptions/6f680d2a-6047-4286-876f-c0ec0b1ff1d0/resourceGroups/resource_group_h8y7t/providers/Microsoft.Compute/virtualMachines/mdgiga28b2acro/extensions/SampleCustomScript",
+       "id":"/subscriptions/"+subscription_id+"/resourceGroups/"+resource_group_name+"/providers/Microsoft.Compute/virtualMachines/"+vm_name+"/extensions/"+custom_script_name,
        "name": custom_script_name,
        "type": "Microsoft.Compute/virtualMachines/extensions",
        "location": location,
@@ -60,24 +60,3 @@ def create_custom_script(**_):
     response_as = requests.put(url=custom_script_url, data=custom_script_params, headers=headers) 
     print response_as.text 
     
-    
-@operation
-def delete_custom_script(**_):
-    resource_group_name = ctx.instance.runtime_properties[constants.RESOURCE_GROUP_KEY]
-    headers, location, subscription_id = auth.get_credentials()
-    custom_script_name = ''
-    vm_name = ''
-    delete_url = 'https://management.azure.com/subscriptions/'+subscription_id+'/resourceGroups/'+resource_group_name+'/providers/Microsoft.Compute/virtualMachines/'+vm_name+'/extensions/'+custom_script_name+'?api-version=2015-05-01-preview'
-    response_as = requests.delete(url=delete_url, headers=headers)
-    print(response_as.text)
-    
-    
-@operation
-def get_custom_script_info(**_):
-    resource_group_name = ctx.instance.runtime_properties[constants.RESOURCE_GROUP_KEY]
-    headers, location, subscription_id = auth.get_credentials()
-    custom_script_name = ''
-    vm_name = ''
-    get_info_url = 'https://management.azure.com/subscriptions/'+subscription_id+'/resourceGroups/'+resource_group_name+'/providers/Microsoft.Compute/virtualMachines/'+vm_name+'/extensions/'+custom_script_name+'?api-version=2015-05-01-preview'
-    response_as = requests.get(url=get_info_url, headers=headers)
-    print(response_as.text)
