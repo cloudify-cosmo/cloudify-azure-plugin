af0c6728113d2808dfeb7f3e6307ae679c87aaa1
Pranjali <pranjalib@users.noreply.github.com>
2015-11-15 01:27:24 -0500

    Update vnet.py

--- azurecloudify/vnet.py
+++ azurecloudify/vnet.py
@@ -48,11 +48,6 @@ def create_vnet(**_):
     resource_group_name = ctx.instance.runtime_properties[constants.RESOURCE_GROUP_KEY]
     if constants.VNET_KEY not in ctx.instance.runtime_properties:
         ctx.instance.runtime_properties[constants.VNET_KEY] = vnet_name 
-    else:
-        random_suffix_value = utils.random_suffix_generator()
-        vnet_name = constants.VNET_PREFIX+random_suffix_value
-        
-    
     
     check_vnet_url = constants.azure_url+'/subscriptions/'+subscription_id+'/resourceGroups/'+resource_group_name+'/providers/microsoft.network/virtualNetworks/'+vnet_name+'?api-version='+constants.api_version_network
     create_vnet_url = constants.azure_url+'/subscriptions/'+subscription_id+'/resourceGroups/'+resource_group_name+'/providers/microsoft.network/virtualNetworks/'+vnet_name+'?api-version='+constants.api_version_network
