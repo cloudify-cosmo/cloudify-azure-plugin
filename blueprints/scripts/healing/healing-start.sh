e2d238da6490345af106d3b9a3627cc68e3f7c7a
tamirko <tamir@gigaspaces.com>
2015-12-27 15:57:00 +0200

    replaced whoami with root

--- blueprints/scripts/healing/healing-start.sh
+++ blueprints/scripts/healing/healing-start.sh
@@ -6,9 +6,9 @@ NTM="$(ctx node properties nodes_to_monitor)"
 ctx logger info "nodes_to_monitor = ${NTM}"
 NTM=$(echo ${NTM} | sed "s/u'/'/g")
 DPLID=$(ctx deployment id)
-currVenv=/home/`whoami`/${DPLID}/env
+currVenv=/root/${DPLID}/env
 ctx logger info "deployment_id = ${DPLID}, virtual env is ${currVenv}"
-pipPath=/home/`whoami`/${DPLID}/env/bin/pip
+pipPath=${currVenv}/bin/pip
 ctx logger info "Running ${pipPath} install influxdb  ... "
 ${pipPath} install influxdb
 statusCode=$?
@@ -21,7 +21,7 @@ ctx logger info "Downloading scripts/healing/policy.py ..."
 LOC=$(ctx download-resource scripts/healing/policy.py)
 
 #nohup "${currVenv}/bin/python" ${LOC} "${NTM}" "${DPLID}" > /dev/null 2>&1 &
-COMMAND="${currVenv}/python ${LOC} \"${NTM}\" ${DPLID} > /home/`whoami`/logfile"
+COMMAND="${currVenv}/python ${LOC} \"${NTM}\" ${DPLID} > /root/logfile"
 crontab_file=/home/`whoami`/mycron
 ctx logger info "Adding ${COMMAND} to ${crontab_file} ..."
 echo "*/1 * * * * $COMMAND" >> ${crontab_file}
