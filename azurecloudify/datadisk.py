c13a13b467fcaffc5ade9608abadd5955262f62f
Ruchika Satalkar <ruchika.satalkar@gmail.com>
2016-02-25 01:32:03 -0500

    Update datadisk.py

--- azurecloudify/datadisk.py
+++ azurecloudify/datadisk.py
@@ -36,15 +36,15 @@ def creation_validation(**_):
 def create_disk(**_):
 	luns = ctx.node.properties['lun']
 	for x in range(0, luns):
-		curr_disk_key = "{0}{1}{2}".format(constants.DATA_DISK_KEY, ctx.node.id, ctx.instance.id)
+		curr_disk_key.append("{0}{1}{2}".format(constants.DATA_DISK_KEY, ctx.node.id, ctx.instance.id))
 		#if curr_disk_key in ctx.instance.runtime_properties:
 		#	ctx.logger.info("Using disk {0}".format(ctx.instance.runtime_properties[curr_disk_key]))
 		#	return
 
 		utils.set_runtime_properties_from_file()
 		current_disk_name = constants.DATA_DISK_PREFIX+utils.random_suffix_generator()
-		ctx.instance.runtime_properties[curr_disk_key] = current_disk_name
-		ctx.logger.info("create_disk: {0} is {1}".format(curr_disk_key, current_disk_name))
+		ctx.instance.runtime_properties[curr_disk_key[x]] = current_disk_name
+		ctx.logger.info("create_disk: {0} is {1}".format(curr_disk_key[x], current_disk_name))
 		ctx.instance.runtime_properties[constants.DATA_DISK_SIZE_KEY] = ctx.node.properties[constants.DATA_DISK_SIZE_KEY]
 		ctx.logger.info("create_disk: {0} is {1}".format(current_disk_name, ctx.instance.runtime_properties[constants.DATA_DISK_SIZE_KEY]))
 
