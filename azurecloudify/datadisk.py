b5bd7ae08f6091b00f62b6ff5fa897fcb365cc3e
Ruchika Satalkar <ruchika.satalkar@gmail.com>
2016-02-28 18:32:25 -0500

    Update datadisk.py

--- azurecloudify/datadisk.py
+++ azurecloudify/datadisk.py
@@ -34,20 +34,19 @@ def creation_validation(**_):
 
 @operation
 def create_disk(**_):
-        disks = ctx.node.properties['no_of_disks']
-        curr_disk_key = []
-        for x in range(0, disks):
-                curr_disk_key.append("{0}{1}{2}{3}".format(constants.DATA_DISK_KEY, ctx.node.id, ctx.instance.id, str(x)))
-                #if curr_disk_key in ctx.instance.runtime_properties:
-                #       ctx.logger.info("Using disk {0}".format(ctx.instance.runtime_properties[curr_disk_key]))
-                #       return
 
-                utils.set_runtime_properties_from_file()
-                current_disk_name = constants.DATA_DISK_PREFIX+utils.random_suffix_generator()
-                ctx.instance.runtime_properties[curr_disk_key[x]] = current_disk_name
-                ctx.logger.info("create_disk: {0} is {1}".format(curr_disk_key[x], current_disk_name))
-                ctx.instance.runtime_properties[constants.DATA_DISK_SIZE_KEY] = ctx.node.properties[constants.DATA_DISK_SIZE_KEY]
-                ctx.logger.info("create_disk: {0} is {1}".format(current_disk_name, ctx.instance.runtime_properties[constants.DATA_DISK_SIZE_KEY]))
+    curr_disk_key = "{0}{1}{2}".format(constants.DATA_DISK_KEY, ctx.node.id, ctx.instance.id)
+    if curr_disk_key in ctx.instance.runtime_properties:
+        ctx.logger.info("Using disk {0}".format(ctx.instance.runtime_properties[curr_disk_key]))
+        return
+
+    utils.set_runtime_properties_from_file()
+    current_disk_name = constants.DATA_DISK_PREFIX+utils.random_suffix_generator()
+    ctx.instance.runtime_properties[curr_disk_key] = current_disk_name
+    ctx.logger.info("create_disk: {0} is {1}".format(curr_disk_key, current_disk_name))
+    ctx.instance.runtime_properties[constants.DATA_DISK_SIZE_KEY] = ctx.node.properties[constants.DATA_DISK_SIZE_KEY]
+    ctx.logger.info("create_disk: {0} is {1}".format(current_disk_name,
+                                                     ctx.instance.runtime_properties[constants.DATA_DISK_SIZE_KEY]))
 
 
 @operation
@@ -58,6 +57,5 @@ def delete_disk(**_):
 @operation
 def set_storageaccount_details(azure_config, **kwargs):
     utils.write_target_runtime_properties_to_file([constants.RESOURCE_GROUP_KEY, constants.STORAGE_ACCOUNT_KEY]+constants.REQUIRED_CONFIG_DATA)
-
 
 
