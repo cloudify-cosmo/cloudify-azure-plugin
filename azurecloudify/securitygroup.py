dd893360511fd0900a8036df87cb15bab7090538
Pranjali <pranjalib@users.noreply.github.com>
2016-02-29 20:41:43 -0500

    Update securitygroup.py

--- azurecloudify/securitygroup.py
+++ azurecloudify/securitygroup.py
@@ -120,7 +120,7 @@ def _get_security_group_params(security_group_json):
     # Issue #30 :
     # https://github.com/cloudify-cosmo/cloudify-azure-plugin/issues/30
     ######################################################################
-    for rule in ctx.node.properties.["rules"]:
+    for rule in ctx.node.properties.[rules]:
         protocol = rule['security_group_protocol']
         source_port_range = rule['security_group_sourcePortRange']
         destination_port_range = rule['security_group_destinationPortRange']
