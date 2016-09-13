49503529f30aeedb3f4f5706b3ecc2abf6b5604e
earthmant <earthmant@icloud.com>
2016-09-13 17:04:43 +0300

    updating to dev version

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ from setuptools import setup
 
 setup(
     name='cloudify-azure-plugin',
-    version='1.4',
+    version='1.4.1.dev0',
     license='LICENSE',
     packages=[
         'cloudify_azure',
