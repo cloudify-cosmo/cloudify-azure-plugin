06e070b2201f6b3c838df6d20ae69fcfc2f9cff3
earthmant <earthmant@icloud.com>
2017-01-24 20:47:16 +0200

    bumping version 1.4.2

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ from setuptools import setup
 
 setup(
     name='cloudify-azure-plugin',
-    version='1.4.1',
+    version='1.4.2',
     license='LICENSE',
     packages=[
         'cloudify_azure',
