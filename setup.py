850867ed60a64b37a5b697e2b6a458a54c4b58ad
Denis Pauk <pauk.denis@gmail.com>
2018-01-19 11:11:48 +0200

    revert version

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ from setuptools import setup
 
 setup(
     name='cloudify-azure-plugin',
-    version='1.9.0',
+    version='1.4.3',
     license='LICENSE',
     packages=[
         'cloudify_azure',
