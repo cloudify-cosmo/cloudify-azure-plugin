2fade9df27ae48c18d56fea4a78c29b6181b6b64
earthmant <trammell@cloudify.co>
2019-12-24 17:04:59 +0200

    add test

---
+++ .cicd/test_manager.py
@@ -0,0 +1,63 @@
+# #######
+# Copyright (c) 2019 Cloudify Platform Ltd. All rights reserved
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+
+from integration_tests.tests.test_cases import PluginsTest
+from integration_tests.tests import utils as test_utils
+
+PLUGIN_NAME = 'cloudify-azure-plugin'
+
+
+class AzurePluginTestCase(PluginsTest):
+
+    base_path = os.path.dirname(os.path.realpath(__file__))
+
+    @property
+    def plugin_root_directory(self):
+        return os.path.abspath(os.path.join(self.base_path, '..'))
+
+    @property
+    def inputs(self):
+        return {
+            'azure_subscription_id': os.getenv('azure_subscription_id'),
+            'azure_tenant_id': os.getenv('azure_tenant_id'),
+            'azure_client_id': os.getenv('azure_client_id'),
+            'azure_client_secret': os.getenv('azure_client_secret'),
+            'agent_key_private': None,
+            'agent_key_public': None,
+            'location': os.getenv('azure_location')
+        }
+
+    @property
+    def wagon_build_time_limit(self):
+        return 1800
+
+    def check_main_blueprint(self):
+        blueprint_id = 'azure_blueprint'
+        dep, ex_id = self.deploy_application(
+            test_utils.get_resource(
+                os.path.join(
+                    self.plugin_root_directory,
+                    'examples/vm.yaml')),
+            timeout_seconds=400,
+            blueprint_id=blueprint_id,
+            deployment_id=blueprint_id,
+            inputs=self.inputs)
+        self.undeploy_application(dep.id)
+
+    def test_blueprints(self):
+        self.upload_mock_plugin(PLUGIN_NAME, self.plugin_root_directory)
+        self.check_main_blueprint()
