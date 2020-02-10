f55929a47bfd4370cbaee80681be7e4cd62b4a82
earthmant <trammell@cloudify.co>
2020-02-10 20:14:04 +0200

    update test

--- .cicd/test_manager.py
+++ .cicd/test_manager.py
@@ -22,6 +22,8 @@ PLUGIN_NAME = 'cloudify-azure-plugin'
 DEVELOPMENT_ROOT = os.path.abspath(
     os.path.join(os.path.dirname(
         os.path.realpath(__file__)), '../..'))
+TEST_KEY_PATH = '/tmp/foo.rsa'
+TEST_PUB_PATH = '/tmp/foo.rsa.pub'
 
 
 class AzurePluginTestCase(PluginsTest):
@@ -43,13 +45,15 @@ class AzurePluginTestCase(PluginsTest):
 
     def create_secrets(self):
         secrets = {
+            'agent_key_private': os.getenv('agent_key_private',
+                                           open(TEST_KEY_PATH).read()),
+            'agent_key_public': os.getenv('agent_key_public',
+                                          open(TEST_PUB_PATH).read()),
             'azure_subscription_id': os.getenv('azure_subscription_id'),
             'azure_tenant_id': os.getenv('azure_tenant_id'),
             'azure_client_id': os.getenv('azure_client_id'),
             'azure_client_secret': os.getenv('azure_client_secret'),
-            'azure_location': os.getenv('azure_location'),
-            'agent_key_private': os.getenv('agent_key_private'),
-            'agent_key_public': os.getenv('agent_key_public'),
+            'azure_location': os.getenv('azure_location', 'westeurope'),
         }
         self._create_secrets(secrets)
 
