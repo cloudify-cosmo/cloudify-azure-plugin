e70137e0f152b4887308404812c4ffc57762449f
nelynehemia <nely@cloudify.co>
2022-11-14 12:35:20 +0200

    fix ctx.plugin

--- cloudify_azure/decorators.py
+++ cloudify_azure/decorators.py
@@ -117,8 +117,10 @@ def with_generate_name(resource_class_name):
             ctx = kwargs['ctx']
             try:
                 # check if name is set or not and generate one if it wasn't set
-                azure_config = utils.get_client_config(ctx.node.properties,
-                                                       ctx.plugin.properties)
+                config_from_plugin_props = getattr(ctx.plugin, 'properties',
+                                                   {})
+                azure_config = utils.get_client_config(
+                    ctx.node.properties, config_from_plugin_props)
                 resource = resource_class_name(azure_config, ctx.logger)
                 name = utils.get_resource_name(ctx)
                 resource_group_name = name
